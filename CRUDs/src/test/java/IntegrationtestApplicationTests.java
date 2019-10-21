3e0cfa838e88ab5bf8f5f7dc291b4c8ab1094660
pachniar <01130999@pw.edu.pl>
2019-10-21 18:24:49 +0200

    Testy na update w nowej klasie - UpdateTest

--- CRUDs/src/test/java/IntegrationtestApplicationTests.java
+++ CRUDs/src/test/java/IntegrationtestApplicationTests.java
@@ -9,7 +9,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CRUDsApp.class)
 public class IntegrationtestApplicationTests {
